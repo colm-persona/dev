a21c3aa4aa228757455787e22f1d2570a6b7dfad
Colm Lang <colm@persona-ai.ai>
2025-02-23 17:33:41 +0000

    good bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,34 @@
 #!/usr/bin/env bash
 
-git clone git@github.com:persona-ae/persona.git
+if [ ! -d "persona" ]; then
+  echo "Cloning persona"
+  git clone git@github.com:persona-ae/persona.git
+else
+  echo "The persona repo already exists"
+fi
 
-git clone git@github.com:colmpat/dev.git
+if [ ! -d "dev" ]; then
+  echo "Cloning dev"
+  git clone git@github.com:colmpat/dev.git
+else
+  echo "The dev repo already exists"
+fi
+
+
+cp ./colm-dev-Dockerfile.dev persona/dockerfiles/
+
+EXCLUDE_FILE="persona/.git/info/exclude"
+PATTERN="^colm-dev"
+
+# Ensure the exclude file exists
+mkdir -p "$(dirname "$EXCLUDE_FILE")"
+touch "$EXCLUDE_FILE"
+
+# Check if "colm-dev" is already in the exclude file
+if ! grep -q "$PATTERN" "$EXCLUDE_FILE"; then
+  echo "Appending 'colm-dev*' to $EXCLUDE_FILE"
+  echo "colm-dev*" >> "$EXCLUDE_FILE"
+else
+  echo "'colm-dev' already exists in $EXCLUDE_FILE"
+fi
+
